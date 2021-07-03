81363e51c317760ae82679f340d6145ca25fd885
Gilmar <66905710+Gilfsantos@users.noreply.github.com>
2021-07-03 18:11:04 -0300

    starting

---
+++ src/main/java/com/gilcadastro/personregistration/PersonregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.gilcadastro.personregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonregistrationApplication.class, args);
+	}
+
+}

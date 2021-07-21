5406732d8bc1085cd70c3082b7cd0019836e5055
Gilmar <66905710+Gilfsantos@users.noreply.github.com>
2021-07-20 21:10:54 -0300

    init project api java

---
+++ src/main/java/com/gilcadastro/personregistration/enums/PhoneType.java
@@ -0,0 +1,17 @@
+package com.gilcadastro.personregistration.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+        // HOME(description: "Home"),
+        // MOBILE(description: "Mobile"),
+        // COMMERCIAL(description: "Comercial");
+
+    //private static String description;
+     // private  final String description ;
+}
